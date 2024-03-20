9f98253455d21e02fc151cb713e9c3f22a0fd2b9
Adventune <adventunemusics@gmail.com>
2024-03-20 13:53:16 +0200

    feat: dummy led blink program

---
+++ src/LedBlink/LedBlink.ino
@@ -0,0 +1,14 @@
+#define LED_BUILTIN 2
+#define DELAY 1000
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(DELAY);
+  digitalWrite(LED_BUILTIN, LOW);
+      delay(DELAY);
+}
+
